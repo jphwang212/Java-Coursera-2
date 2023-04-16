553af4e34f4bfaca8b574ea68217490973ab20a7
jphwang212 <jphwang212@gmail.com>
2023-04-16 09:57:02 -0400

    completed first part

--- src/WordLengths.java
+++ src/WordLengths.java
@@ -4,9 +4,15 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.File;
 
 public class WordLengths {
-//    public int indexOfMax(int[] values){
-//
-//    }
+    public int indexOfMax(int[] values){
+        int max = 0;
+        for(int i = 0; i < values.length; i++){
+            if(values[i] > max){
+                max = values[i];
+            }
+        }
+        return max;
+    }
     public void countWordLengths(FileResource resource, int[] counts){
         for(String word : resource.words()){
             int length = word.length();
@@ -21,12 +27,16 @@ public class WordLengths {
             }
             counts[length]++;
         }
-        System.out.println(counts.toString());
+        for(int i = 1; i < counts.length; i++){
+            System.out.println(i + " letter word: " + counts[i]);
+        }
     }
     public void testCountWordLengths(){
         FileResource fr = new FileResource();
         int[] counts = new int[31];
         countWordLengths(fr, counts);
+        int max = indexOfMax(counts);
+        System.out.println("Largest element at: " + max);
     }
 
     public static void main(String[] args) {
