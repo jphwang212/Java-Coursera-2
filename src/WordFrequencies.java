1de5ed51cbb071f23d433e2e18e57b5a9633208c
jphwang212 <jphwang212@gmail.com>
2023-04-30 18:08:28 -0400

    requires testing

--- src/WordFrequencies.java
+++ src/WordFrequencies.java
@@ -1,4 +1,8 @@
+import edu.duke.FileResource;
+
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class WordFrequencies {
     private ArrayList<String> myWords;
@@ -8,6 +12,40 @@ public class WordFrequencies {
         this.myFreqs = freqs;
     }
     public void findUnique(){
+        this.myWords.clear();
+        this.myFreqs.clear();
+        FileResource fr = new FileResource();
+        for(String s : fr.words()){
+            if(!myWords.contains(s)){
+                this.myWords.add(s);
+            }
+            int wordIdx = this.myWords.indexOf(s);
+            int nextValue = this.myFreqs.get(wordIdx);
+            this.myFreqs.set(wordIdx, nextValue + 1);
+        }
+    }
+    public int findIndexOfMax(){
+        int maxIdx = 0;
+        for(int i = 1; i < this.myFreqs.size(); i++){
+            if(this.myFreqs.get(i) > this.myFreqs.get(maxIdx)){
+                maxIdx = i;
+            }
+        }
+        return maxIdx;
+    }
+    public void tester(){
+        findUnique();
+        int maxIdx = findIndexOfMax();
+        for(String s : this.myWords){
+            int idx = this.myFreqs.indexOf(s);
+            int freq = this.myFreqs.get(idx);
+            System.out.println(s + ": " + freq);
+        }
+        System.out.println("Index of max frequency: " + maxIdx);
+    }
 
+    public static void main(String[] args) {
+        WordFrequencies inst = new WordFrequencies();
+        inst.tester();
     }
 }
